c187d599a3394fefdc044a5638d0bcced679804d
Patrick <pfontani@ucsc.edu>
2025-01-09 15:33:40 -0800

    first commit

---
+++ Assignments/ASG0/asg0.js
@@ -0,0 +1,18 @@
+import Vector3 from "lib/cuon-matrix-cse160.js";
+
+function main() {
+  // retrieve <canvas> element
+  var canvas = document.getElementById("example");
+
+  if (!canvas) {
+    console.log("Failed to retrieve the <canvas> element");
+    return;
+  }
+
+  // get rendering context for 2dcg
+  var ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "rgba(0, 0, 255, 1.0)"; // set a blue color
+  ctx.fillRect(120, 10, 150, 150); // fill a rectangle with the color
+  
+}
