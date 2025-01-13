2af349da4a92f0c47a4417f97dd3070aa2bc6164
Patrick <pfontani@ucsc.edu>
2025-01-13 14:57:22 -0800

    got coordinates to work for both vectors

--- Assignments/ASG0/asg0.js
+++ Assignments/ASG0/asg0.js
@@ -15,6 +15,7 @@ function main() {
   ctx.fillRect(0, 0, 400, 400); // fill a rectangle with the color
 }
 
+
 function clearRect() {
   var canvas = document.getElementById("example");
   var ctx = canvas.getContext("2d");
@@ -24,23 +25,34 @@ function clearRect() {
 }
 
 
-function handleDrawEvent() {
-  clearRect();
-  const x = document.getElementById("x");
-  const y = document.getElementById("y");
-  const v1 = new Vector3([x.value, y.value, 0]);
+function drawVector(v, color) {
   var canvas = document.getElementById("example");
   const cx = canvas.width / 2;
   const cy = canvas.height / 2;
   var ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, 400, 400)
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, 400, 400);
-  ctx.strokeStyle = "red";
+
+  // draw the first vector
+  ctx.strokeStyle = color;
   ctx.beginPath();
   ctx.moveTo(cx, cy);
-  ctx.lineTo(cx - (v1.elements[0] * 20), cy + (v1.elements[1] * 20));
+  ctx.lineTo(cx + (v.elements[0] * 20), cy - (v.elements[1] * 20));
   ctx.stroke();
+
+}
+
+
+function handleDrawEvent() {
+  clearRect();
+  const x1 = document.getElementById("x1");
+  const y1 = document.getElementById("y1");
+  const x2 = document.getElementById("x2");
+  const y2 = document.getElementById("y2");
+  const v1 = new Vector3([x1.value, y1.value, 0]);
+  const v2 = new Vector3([x2.value, y2.value, 0]);
+
+  drawVector(v1, "red");
+  drawVector(v2, "blue");
+
 }
 
 
