5ae7fb3959ad66833bce5243399056e205f42795
Patrick <pfontani@ucsc.edu>
2025-01-13 14:47:37 -0800

    made input

--- Assignments/ASG0/asg0.js
+++ Assignments/ASG0/asg0.js
@@ -11,26 +11,40 @@ function main() {
 
   // // get rendering context for 2dcg
   var ctx = canvas.getContext("2d");
-  ctx.fillStyle = "black"; // set a blue color
+  ctx.fillStyle = "black";
   ctx.fillRect(0, 0, 400, 400); // fill a rectangle with the color
-  const v1 = new Vector3([2.25,2.25,0]);
-  drawVector(v1, "red");
+}
+
+function clearRect() {
+  var canvas = document.getElementById("example");
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, 400, 400)
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 400, 400);
 }
 
 
-function drawVector(v, color) {
+function handleDrawEvent() {
+  clearRect();
+  const x = document.getElementById("x");
+  const y = document.getElementById("y");
+  const v1 = new Vector3([x.value, y.value, 0]);
   var canvas = document.getElementById("example");
   const cx = canvas.width / 2;
   const cy = canvas.height / 2;
   var ctx = canvas.getContext("2d");
-  ctx.strokeStyle = color;
+  ctx.clearRect(0, 0, 400, 400)
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 400, 400);
+  ctx.strokeStyle = "red";
   ctx.beginPath();
   ctx.moveTo(cx, cy);
-  ctx.lineTo(cx - (v.elements[0] * 20), cy + (v.elements[1] * 20));
+  ctx.lineTo(cx - (v1.elements[0] * 20), cy + (v1.elements[1] * 20));
   ctx.stroke();
-
 }
 
 
 
 main()
+const draw = document.getElementById("draw")
+draw.addEventListener("click", handleDrawEvent)
