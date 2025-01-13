7f73c59f8274a8e3685bcf114032ed0b0e00611c
Patrick <pfontani@ucsc.edu>
2025-01-13 14:06:29 -0800

    got black rectangle to appear

--- Assignments/ASG0/asg0.js
+++ Assignments/ASG0/asg0.js
@@ -12,14 +12,22 @@ function main() {
   // // get rendering context for 2dcg
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "black"; // set a blue color
-  ctx.fillRect(120, 10, 150, 150); // fill a rectangle with the color
+  ctx.fillRect(0, 0, 400, 400); // fill a rectangle with the color
   const v1 = new Vector3([1,2,0]);
-
+  drawVector(v1, "red");
 }
 
 
-// function drawVector(v, color) {
+function drawVector(v, color) {
+  console.log(v, color);
+  var canvas = document.getElementById("example");
+  var ctx = canvas.getContext("2d");
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2, canvas.height / 2);
+  ctx.lineTo(220, 220);
+  ctx.stroke();
 
-// }
+}
 
 main();
