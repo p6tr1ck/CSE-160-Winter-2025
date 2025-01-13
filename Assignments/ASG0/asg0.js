dc994f64655d361719e66beb747ee201f3726b8d
Patrick <pfontani@ucsc.edu>
2025-01-13 14:26:33 -0800

    .

--- Assignments/ASG0/asg0.js
+++ Assignments/ASG0/asg0.js
@@ -13,21 +13,24 @@ function main() {
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "black"; // set a blue color
   ctx.fillRect(0, 0, 400, 400); // fill a rectangle with the color
-  const v1 = new Vector3([1,2,0]);
+  const v1 = new Vector3([2.25,2.25,0]);
   drawVector(v1, "red");
 }
 
 
 function drawVector(v, color) {
-  console.log(v, color);
   var canvas = document.getElementById("example");
+  const cx = canvas.width / 2;
+  const cy = canvas.height / 2;
   var ctx = canvas.getContext("2d");
   ctx.strokeStyle = color;
   ctx.beginPath();
-  ctx.moveTo(canvas.width / 2, canvas.height / 2);
-  ctx.lineTo(220, 220);
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(cx - (v.elements[0] * 20), cy + (v.elements[1] * 20));
   ctx.stroke();
 
 }
 
-main();
+
+
+main()
