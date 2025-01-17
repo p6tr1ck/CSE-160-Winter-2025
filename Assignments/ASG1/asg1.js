d50fd8aef9e1a4fecc1d06e5d354a9c4c38d2a66
Patrick <pfontani@ucsc.edu>
2025-01-17 12:46:01 -0800

    finished 1.5

--- Assignments/ASG1/asg1.js
+++ Assignments/ASG1/asg1.js
@@ -1,12 +1,13 @@
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
+  uniform float u_Size;
   void main() {
     gl_Position = a_Position;
-    gl_PointSize = 20.0;
+    gl_PointSize = u_Size;
   }`
 
 var FSHADER_SOURCE = `
-  precision mediump float;
+  precision mediump float; 
   uniform vec4 u_FragColor;
   void main() {
     gl_FragColor = u_FragColor;
@@ -18,6 +19,7 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
+let u_Size;
 
 
 function setupWebGL() {
@@ -49,10 +51,17 @@ function connectVariablesToGLSL() {
     console.log('Failed to get the storage location of u_FragColor');
     return;
   }
+
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
 }
 
 
 let g_selectedColor = [1.0,1.0,1.0,1.0];
+let g_selectedSize = 5;
 
 function addActionsForHtmlUI() {
   document.getElementById('green').onclick = function() { g_selectedColor = [0.0,1.0,0.0,1.0]; };
@@ -62,17 +71,20 @@ function addActionsForHtmlUI() {
   document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
   document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });
 
+  document.getElementById('sizeSlide').addEventListener('mouseup', function () { g_selectedSize = this.value });
 }
 
 
 
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
+var g_sizes = [];
 function click(ev) {
   [x,y] = convertCoordinatesEventToGL(ev);
 
   g_points.push([x,y]);
   g_colors.push(g_selectedColor.slice());
+  g_sizes.push(g_selectedSize);
   // if (x >= 0.0 && y >= 0.0) {
   //   g_colors.push([1.0, 0.0, 0.0, 1.0])
   // } else if (x < 0.0 && y < 0.0) {
@@ -104,9 +116,11 @@ function renderAllShapes() {
   for (var i = 0; i < len; i++) {
     var xy = g_points[i];
     var rgba = g_colors[i];
+    var size = g_sizes[i];
 
     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    gl.uniform1f(u_Size, size);
     gl.drawArrays(gl.POINTS, 0, 1);
   }
 }
