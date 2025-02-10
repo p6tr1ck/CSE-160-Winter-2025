0c833c44348183d046321150b3677f08528887a7
Patrick <pfontani@ucsc.edu>
2025-02-09 16:15:43 -0800

    3.6

--- Assignments/ASG3/blockyAnimal2/World.js
+++ Assignments/ASG3/blockyAnimal2/World.js
@@ -266,14 +266,30 @@ function convertCoordinatesEventToGL(ev) {
   return [x, y];
 }
 
+var g_eye = [0, 0, 3];
+var g_at = [0, 0, -100];
+var g_up = [0, 1, 0];
+
 // draw all the cubes in one place here
 function renderScene() {
   var startTime = performance.now();
 
   var projMat = new Matrix4();
+  projMat.setPerspective(50, canvas.width / canvas.height, 1, 100);
   gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
 
   var viewMat = new Matrix4();
+  viewMat.setLookAt(
+    g_eye[0],
+    g_eye[1],
+    g_eye[2],
+    g_at[0],
+    g_at[1],
+    g_at[2],
+    g_up[0],
+    g_up[1],
+    g_up[2]
+  );
   gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements);
 
   var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
